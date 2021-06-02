97eb4cfbfee1407c19f4ada6e8d43cfab9c6a7e7
Andrew1707227 <1707227@mukilteo.wednet.edu>
2021-06-02 15:48:02 -0700

    Added player movement, death animation, and trails

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float minSpeed;
+    private float currSpeed;
+    public float maxSpeed;
+    public float accler;
+
+    private bool debounce;
+
+    void Start() {
+        currSpeed = minSpeed;
+        debounce = false;
+    }
+
+    void FixedUpdate() {
+        float rot = transform.rotation.eulerAngles.z;
+        if (Input.GetAxis("Horizontal") == 1 && rot != 90) {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 270));
+            if (currSpeed < maxSpeed) {
+                currSpeed += accler;
+            }
+        } else if (Input.GetAxis("Horizontal") == -1 && rot != 270) {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+            if (currSpeed < maxSpeed) {
+                currSpeed += accler;
+            } 
+        } else if (Input.GetAxis("Vertical") == 1 && rot != 180) {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            if (currSpeed < maxSpeed) {
+                currSpeed += accler;
+            } 
+        }else if (Input.GetAxis("Vertical") == -1 && rot != 0) {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
+            if (currSpeed < maxSpeed) {
+                currSpeed += accler;
+            } 
+        } else if (currSpeed > minSpeed) {
+            currSpeed -= accler;
+        }
+        rot = transform.rotation.eulerAngles.z;
+        if (rot == 0) {
+            transform.position += Vector3.up * currSpeed;
+        } else if (rot == 90) {
+            transform.position += Vector3.left * currSpeed;
+        } else if (rot == 180) {
+            transform.position += Vector3.down * currSpeed;
+        } else if (rot == 270) {
+            transform.position += Vector3.right * currSpeed;
+        }
+    }
+    public float getSpeed() {
+        return currSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Wall" && !debounce) {
+            debounce = true;
+            StartCoroutine(killPlayer());
+        }
+    }
+
+    private IEnumerator killPlayer() {
+        GetComponent<Animator>().enabled = true;
+        maxSpeed = 0;
+        minSpeed = 0;
+        currSpeed = 0;
+        yield return new WaitForSeconds(.25f);
+        GetComponent<SpriteRenderer>().enabled = false;
+        yield return new WaitForSeconds(.25f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
---
+++ Assets/Scripts/TrailScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailScript : MonoBehaviour {
+    public GameObject trailPrefab;
+    public Color trailColor;
+
+    private GameObject currTrail;
+    private float currRot;
+    private Vector3 trailPos;
+
+    void Start() {
+        currRot = 0;
+        SpawnTrail();
+    }
+
+    void FixedUpdate() {
+        if (currRot != transform.rotation.eulerAngles.z) {
+            SpawnTrail();
+        }
+        if (TryGetComponent(out PlayerMove playerMove)) {
+            currTrail.transform.position = trailPos + (transform.position - trailPos) / 2;
+            currTrail.transform.localScale += Vector3.up * GetComponent<PlayerMove>().getSpeed();
+        } else {
+            //currTrail.transform.localScale += Vector3.up * GetComponent<EnemyMove>().getSpeed();
+        }
+        currRot = transform.rotation.eulerAngles.z;
+    }
+
+    private void SpawnTrail() {
+        if (currTrail) {
+            currTrail.transform.localScale -= Vector3.up * GetComponent<PlayerMove>().getSpeed();
+        }
+        currTrail = Instantiate(trailPrefab, transform.position, transform.rotation);
+        trailPos = transform.position;
+        currTrail.GetComponent<SpriteRenderer>().color = trailColor;
+    }
+}
