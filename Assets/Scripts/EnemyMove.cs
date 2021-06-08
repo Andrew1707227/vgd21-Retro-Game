4ac4bd004c356d98999d603f0768c5914de03dd4
Andrew1707227 <1707227@mukilteo.wednet.edu>
2021-06-08 13:26:23 -0700

    Update Game.unity and EnemyMove.cs

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -13,39 +13,42 @@ public class EnemyMove : MonoBehaviour {
 
     void FixedUpdate() {
         float rot = transform.rotation.eulerAngles.z;
-        RaycastHit2D Yray = Physics2D.Raycast(transform.position,transform.up);
-        RaycastHit2D rightRay = Physics2D.Raycast(transform.position, transform.right);
-        RaycastHit2D leftRay = Physics2D.Raycast(transform.position, -transform.right);
-        Debug.Log(Yray.distance);
-       if (Yray.distance == Mathf.Clamp(Yray.distance,.01f,.75f)) {
-            var rotation = transform.rotation;
-            rotation.eulerAngles += new Vector3(0, 0, 90);
-            transform.rotation = rotation;
-        }
-
-        /*if (bigboi.collider.tag == "Wall") {
-            if (bigboi == rays[3] && rot != 90) {
-                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 270));
-
-            }else if (bigboi == rays[2] && rot != 270) {
-                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
-
-            } else if (bigboi == rays[0] && rot != 180) {
-                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
-
-            } else if (bigboi == rays[1] && rot != 0) {
-                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
-            }
-        }*/
-        rot = transform.rotation.eulerAngles.z;
+        RaycastHit2D ray = Physics2D.Raycast(transform.position,Vector2.zero);
         if (rot == 0) {
             transform.position += Vector3.up * speed;
+            ray = Physics2D.Raycast(transform.position, Vector3.up);
+            float rad = Random.value;
+            if (ray.distance < 1 && rad < .5f) {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+            } else if (ray.distance < 1) {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 270));
+            }
         } else if (rot == 90) {
             transform.position += Vector3.left * speed;
+            float rad = Random.value;
+            if (ray.distance < 1 && rad < .5f) {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
+            } else if (ray.distance < 1) {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            }
         } else if (rot == 180) {
             transform.position += Vector3.down * speed;
+            ray = Physics2D.Raycast(transform.position, Vector3.down);
+            float rad = Random.value;
+            if (ray.distance < 1 && rad < .5f) {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 270));
+            } else if (ray.distance < 1) {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+            }
         } else if (rot == 270) {
             transform.position += Vector3.right * speed;
+            ray = Physics2D.Raycast(transform.position, Vector3.right);
+            float rad = Random.value;
+            if (ray.distance < 1 && rad < .5f) {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            } else if (ray.distance < 1) {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
+            }
         }
     }
 
